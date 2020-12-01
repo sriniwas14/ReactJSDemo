e419a591823dbbaeedbe35078451d9865101e00e
sriniwas14 <sriniwasx@gmail.com>
2020-12-01 14:45:05 +0530

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import Users from './Users';
+import UserSingle from './UserSingle';
 import store from './store';
 import { Provider } from 'react-redux';
 import { Container } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Container>
-          <Users />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Users />
+            </Route>
+            <Route exact path="/users/:id">
+              <UserSingle />
+            </Route>
+          </Switch>
+        </Router>
         </Container>
       </div>
     </Provider>
